372a6e7b50c0cddb399e52beb4ee1a36b652174b
Paulo Moutinho <paulo_vicali@icloud.com>
2022-10-07 12:07:20 -0300

    update dos projetos reais

--- components/Projects.js
+++ components/Projects.js
@@ -10,7 +10,8 @@ function Projects(props) {
       <Stack spacing={8}>
         <ProjectCard
           title='Dev.Finance$'
-          description='Learn how to use Chakra UI like a PRO. The complete course for absolute beginners to understand how Chakra UI works & get started building.'
+          // description='Learn how to use Chakra UI like a PRO. The complete course for absolute beginners to understand how Chakra UI works & get started building.'
+          description="Uma aplicação para controle financeiro simples e rápido. Nela você pode cadastrar suas despesas e entradas. Você pode também visualizar o seu balanço, ou seja, o seu saldo atual."
           src='/assets/finance.png'
           link='https://github.com/moutinhofuturedev/Codigo-Sistema-Financeiro'
           link2='https://devsfinances.netlify.app/'
@@ -18,7 +19,8 @@ function Projects(props) {
         />
         <ProjectCard
           title='Event Platform'
-          description='Created the Responsive UI Design in Figma for all the pages and Developed the entire features of the fully responsive wesbite using Chakra UI and Next.js. '
+          // description='Created the Responsive UI Design in Figma for all the pages and Developed the entire features of the fully responsive wesbite using Chakra UI and Next.js. '
+          description="Com ReactJS e Tailwind, construímos esta fantástica plataforma de eventos onde podemos compartilhar conteúdo por tempo limitado com todos os inscritos do evento."
           src='/assets/event.png'
           link='https://github.com/moutinhofuturedev/Event-Platform'
           link2='https://event-platform-yoqu.vercel.app/'
