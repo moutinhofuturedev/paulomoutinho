d871b588e6e7ab08c7abf60dfa96b525511b205a
Paulo Moutinho <paulo_vicali@icloud.com>
2022-10-09 12:07:25 -0300

    informacoes sobre seo

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,12 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang='pt-BR'>
         <Head>
         <link rel="icon" type="image/svg+xml" href="/assets/PM.svg" />
         <script
           defer
-          data-domain='estheragbaje.dev'
+          data-domain='paulomoutinho.vercel.app'
           src='https://plausible.io/js/plausible.js'
         ></script>
         </Head>
