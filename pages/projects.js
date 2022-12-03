1405bf02e08aafa83cd60a15d79002fa098fddc5
Paulo Moutinho <paulo_vicali@icloud.com>
2022-12-03 11:32:25 -0300

    chore: trabalhando na página de projetos

---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react"
+import Navigation from "../components/Header"
+
+export default function Projects() {
+    return(
+        <Box bg="generalBackground">
+        <Navigation key={1}/>
+        </Box>
+    )
+}
