a4932ea55f840b7565f1acd81639afd27747fc72
Paulo Moutinho <paulo_vicali@icloud.com>
2022-12-03 11:36:16 -0300

    refactor: ajustando estrutura

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Button, Heading, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Button, Heading, SimpleGrid, Text, VStack } from '@chakra-ui/react';
 import Head from 'next/head';
 import React from 'react';
 import AboutMe from '../components/AboutMe';
@@ -28,183 +28,182 @@ function App() {
     <>
       <Head>
         <title>Paulo Moutinho | Portfólio</title>
-        {/* <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          // href='/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          // href='/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          // href='/favicon-16x16.png'
-        /> */}
-        <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='theme-color' content='#ffffff' />
-        <meta name='description' content='Online Portfolio' />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="description" content="Online Portfolio" />
         <meta
-          name='robots'
-          content='max-snippet:-1, max-image-preview:large, max-video-preview:-1'
+          name="robots"
+          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
         />
-        <link rel='canonical' href='https://paulomoutinho.vercel.app/' />
-        <meta property='og:locale' content='pt-BR' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='Paulo Moutinho | Portfolio' />
-        <meta property='og:description' content='Paulo Moutinho Portfolio' />
-        <meta property='og:url' content='https://paulomoutinho.vercel.app/' />
-        <meta property='og:site_name' content='Online Portfolio' />
-        <meta property='og:image' content='/assets/paulomoutinho.jpg' />
+        <link rel="canonical" href="https://paulomoutinho.vercel.app/" />
+        <meta property="og:locale" content="pt-BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Paulo Moutinho | Portfolio" />
+        <meta property="og:description" content="Paulo Moutinho Portfolio" />
+        <meta property="og:url" content="https://paulomoutinho.vercel.app/" />
+        <meta property="og:site_name" content="Online Portfolio" />
+        <meta property="og:image" content="/assets/paulomoutinho.jpg" />
         <meta
-          property='og:image:secure_url'
-          content='/assets/paulomoutinho.jpg'
+          property="og:image:secure_url"
+          content="/assets/paulomoutinho.jpg"
         />
-        <meta property='og:image:width' content='1280' />
-        <meta property='og:image:height' content='720' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:description' content='Paulo Moutinho Portfolio' />
-        <meta name='twitter:title' content='Paulo Moutinho | Portfolio' />
-        <meta name='twitter:image' content='/assets/paulomoutinho.jpg' />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="720" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="Paulo Moutinho Portfolio" />
+        <meta name="twitter:title" content="Paulo Moutinho | Portfolio" />
+        <meta name="twitter:image" content="/assets/paulomoutinho.jpg" />
       </Head>
-      <Box bg='generalBackground' color='bodyText'>
+      <Box bg="generalBackground" color="bodyText">
         <Header />
         <LeftBar />
         <RightBar />
         <Main>
-          <Box paddingY='80px'>
-            <Text color='highlight' mb='20px'>
+          <Box paddingY="80px">
+            <Text color="highlight" mb="20px">
               Olá, meu nome é
             </Text>
-            <Heading as='h1'
-              fontWeight='bold'
-              fontSize={{ base: '32px', md: '60px' }}
-              color='headingText'
+            <Heading
+              as="h1"
+              fontWeight="bold"
+              fontSize={{ base: "32px", md: "60px" }}
+              color="headingText"
             >
               Paulo Moutinho.
               <br></br>Software Engineer 🕹 | Front End
             </Heading>
 
             <Text
-              lineHeight='tall'
-              color='bodyText'
+              lineHeight="tall"
+              color="bodyText"
               mt={8}
-              maxWidth='760px'
-              fontSize='lg'
+              maxWidth="760px"
+              fontSize="lg"
             >
-              Sou um desenvolvedor iniciando uma jornada no mundo da programação e suas tecnologias incríveis.
-              Atualmente, trabalho como Software Engineer Junior na compania Via, atuando com as tecnologias 
-              mais modernas no Front End para melhorar o SEO das páginas de Casas Bahia, Extra e Ponto.
+              Sou um desenvolvedor iniciando uma jornada no mundo da programação
+              e suas tecnologias incríveis. Atualmente, trabalho como Software
+              Engineer Junior na compania Via, atuando com as tecnologias mais
+              modernas no Front End para melhorar o SEO das páginas de Casas
+              Bahia, Extra e Ponto.
             </Text>
             <Button
-              variant='outline'
-              marginY='60px'
-              size='lg'
-              colorScheme='teal'
-              as='a'
-              href='mailto:paulo_vicali@icloud.com'
+              variant="outline"
+              marginY="60px"
+              size="lg"
+              colorScheme="teal"
+              as="a"
+              href="mailto:paulo_vicali@icloud.com"
             >
               Entrar em contato
             </Button>
           </Box>
-          <Box paddingY='150px'>
-            <SectionHeading number='01.' id='videos'>
+          <Box paddingY="150px">
+            <SectionHeading number="01." id="videos">
               Vídeos Favoritos
             </SectionHeading>
             <VideoSection />
             <Button
-              variant='outline'
-              size='lg'
-              textAlign='center'
-              colorScheme='teal'
-              as='a'
-              href='https://www.youtube.com/c/RocketSeat'
-              target='_blank'
+              variant="outline"
+              size="lg"
+              textAlign="center"
+              colorScheme="teal"
+              as="a"
+              href="https://www.youtube.com/c/RocketSeat"
+              target="_blank"
             >
               Veja mais vídeos
             </Button>
           </Box>
-          <Box paddingY='150px'>
-            <SectionHeading number='02.' id='blogs'>
+          <Box paddingY="150px">
+            <SectionHeading number="02." id="blogs">
               Artigos em destaque
             </SectionHeading>
 
-            <SimpleGrid columns={{ base: 1, md: 3 }} spacing='20px'>
+            <SimpleGrid columns={{ base: 1, md: 3 }} spacing="20px">
               <BlogCard
-                link='https://medium.com/viahub/arrdissecando-a-carreira-do-software-engineering-specialist-staff-principal-e-distinguished-cdd92ca3ef1a'
-                title='Dissecando a carreira do Software Engineering Specialist'
+                link="https://medium.com/viahub/arrdissecando-a-carreira-do-software-engineering-specialist-staff-principal-e-distinguished-cdd92ca3ef1a"
+                title="Dissecando a carreira do Software Engineering Specialist"
                 details='A carreira do especialista é uma polêmica aqui no nosso país. Muitos já ouviram falar, mas não existe um "material oficial" destrinchando os papéis...'
-                date='18 Agosto, 2022'
+                date="18 Agosto, 2022"
               />
               <BlogCard
-                link='https://medium.com/viahub/devo-ser-especialista-ou-seguir-pra-lideran%C3%A7a-aab87a59d3aa'
-                title='Devo ser especialista ou seguir pra liderança?'
+                link="https://medium.com/viahub/devo-ser-especialista-ou-seguir-pra-lideran%C3%A7a-aab87a59d3aa"
+                title="Devo ser especialista ou seguir pra liderança?"
                 details="Acredito que quase todo mundo que começa uma carreira, especialmente em tecnologia, tem a dúvida de pra onde ir depois de se tornar sênior...."
-                date='26 Agosto, 2022'
+                date="26 Agosto, 2022"
               />
               <BlogCard
-                link='https://medium.com/viahub/criando-um-framework-para-definir-m%C3%A9trica-de-produto-6e8191bc0c37'
-                title='Criando um framework para definir métrica de produto'
+                link="https://medium.com/viahub/criando-um-framework-para-definir-m%C3%A9trica-de-produto-6e8191bc0c37"
+                title="Criando um framework para definir métrica de produto"
                 details="Recebi o desafio de dar continuidade à construção da arquitetura e experiência do usuário em um sistema ligado ao varejo e na jornada de logística em uma das principais empresas do varejo do país, a Via..."
-                date='13 Maio, 2022'
+                date="13 Maio, 2022"
               />
               <BlogCard
-                link='https://medium.com/viahub/oi-estudar-em-hor%C3%A1rio-de-trabalho-aqui-na-viahub-pode-86a483e2b3b2'
-                title='Oi? Estudar em horário de trabalho? Aqui na ViaHub pode!'
-                details='2021 foi O ANO DA VIAHUB! Criamos nossa marca da Tecnologia chamada ViaHub, nossas plataformas evoluíram MUITO, melhoramos váriossss indicadores, desenvolvemos diversas iniciativas de engajamento...'
-                date='31 Novembro, 2021'
+                link="https://medium.com/viahub/oi-estudar-em-hor%C3%A1rio-de-trabalho-aqui-na-viahub-pode-86a483e2b3b2"
+                title="Oi? Estudar em horário de trabalho? Aqui na ViaHub pode!"
+                details="2021 foi O ANO DA VIAHUB! Criamos nossa marca da Tecnologia chamada ViaHub, nossas plataformas evoluíram MUITO, melhoramos váriossss indicadores, desenvolvemos diversas iniciativas de engajamento..."
+                date="31 Novembro, 2021"
               />
               <BlogCard
-                link='https://medium.com/viahub/reatividade-turbinada-com-vuex-a37145dc0c58'
-                title='Reatividade turbinada com Vuex'
+                link="https://medium.com/viahub/reatividade-turbinada-com-vuex-a37145dc0c58"
+                title="Reatividade turbinada com Vuex"
                 details="Você já se perguntou para que serve o gerenciamento de estados em uma aplicação de frontend? Ou talvez tenha se perguntado como funciona esse gerenciamento..."
-                date='22 Outubro, 2020'
+                date="22 Outubro, 2020"
               />
 
               <BlogCard
-                link='https://medium.com/viahub/ci%C3%AAncia-de-dados-escal%C3%A1vel-na-via-eae755be723a'
-                title='Ciência de Dados Escalável na ViaHub'
+                link="https://medium.com/viahub/ci%C3%AAncia-de-dados-escal%C3%A1vel-na-via-eae755be723a"
+                title="Ciência de Dados Escalável na ViaHub"
                 details="A Via é a plataforma de consumo do brasileiro! Conhecida de longa data pelas suas marcas Casas Bahia e Ponto :>, ela vem se transformando para ajudar a realizar cada vez mais os sonhos dos brasileiros, contando com uma aceleração tecnológica gigante..."
-                date='28 Janeiro, 2022'
+                date="28 Janeiro, 2022"
               />
             </SimpleGrid>
             <Button
-              variant='outline'
-              marginY='60px'
-              size='lg'
-              textAlign='center'
-              colorScheme='teal'
-              as='a'
-              href='https://medium.com/viahub'
-              target='_blank'
+              variant="outline"
+              marginY="60px"
+              size="lg"
+              textAlign="center"
+              colorScheme="teal"
+              as="a"
+              href="https://medium.com/viahub"
+              target="_blank"
             >
               Ver mais artigos
             </Button>
           </Box>
           <AboutMe />
-          <Projects />
-          <Box paddingY='150px' textAlign='center' id='contact'>
+          <VStack alignItems="flex-start">
+            <Projects />
+            <Button
+              variant="outline"
+              size="lg"
+              textAlign="center"
+              colorScheme="teal"
+              as="a"
+              href="/projects"
+            >
+              Ver mais projetos
+            </Button>
+          </VStack>
+          <Box paddingY="150px" textAlign="center" id="contact">
             <Heading>Entrar em contato</Heading>
             <Text
-              paddingY='10px'
-              color='bodyText'
-              fontSize='lg'
-              maxWidth='50ch'
-              mx='auto'
+              paddingY="10px"
+              color="bodyText"
+              fontSize="lg"
+              maxWidth="50ch"
+              mx="auto"
             >
-              Gostaria de se conectar e compartilhar conhecimento? Entre em contato comigo! 📧
+              Gostaria de se conectar e compartilhar conhecimento? Entre em
+              contato comigo! 📧
             </Text>
             <Button
-              as='a'
-              href='mailto:paulo_vicali@icloud.com'
-              variant='outline'
-              marginY='60px'
-              size='lg'
-              colorScheme='teal'
+              as="a"
+              href="mailto:paulo_vicali@icloud.com"
+              variant="outline"
+              marginY="60px"
+              size="lg"
+              colorScheme="teal"
             >
               Enviar email
             </Button>
