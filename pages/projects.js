ce0cf30172f6ba805c583ffa4a59731238cdedbc
Paulo Moutinho <paulo_vicali@icloud.com>
2022-12-04 23:36:36 -0300

    feat: adiciona nova página

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,45 @@
+import Head from 'next/head';
 import { Box } from "@chakra-ui/react"
 import Navigation from "../components/Header"
+import { LeftBar } from "../components/LeftBar"
+import { RightBar } from "../components/RightBar"
 
 export default function Projects() {
-    return(
-        <Box bg="generalBackground">
-        <Navigation key={1}/>
+    return (
+      <>
+        <Head>
+          <title>Paulo Moutinho | Projetos</title>
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="description" content="Online Portfolio" />
+          <meta
+            name="robots"
+            content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+          />
+          <link rel="canonical" href="https://paulomoutinho.vercel.app/" />
+          <meta property="og:locale" content="pt-BR" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Paulo Moutinho | Portfolio" />
+          <meta property="og:description" content="Paulo Moutinho Portfolio" />
+          <meta property="og:url" content="https://paulomoutinho.vercel.app/" />
+          <meta property="og:site_name" content="Online Portfolio" />
+          <meta property="og:image" content="/assets/paulomoutinho.jpg" />
+          <meta
+            property="og:image:secure_url"
+            content="/assets/paulomoutinho.jpg"
+          />
+          <meta property="og:image:width" content="1280" />
+          <meta property="og:image:height" content="720" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:description" content="Paulo Moutinho Portfolio" />
+          <meta name="twitter:title" content="Paulo Moutinho | Portfolio" />
+          <meta name="twitter:image" content="/assets/paulomoutinho.jpg" />
+        </Head>
+        <Box bg="generalBackground" color="bodyText">
+          <Navigation />
+          <LeftBar />
+          <RightBar />
         </Box>
-    )
+      </>
+    );
 }
