de4571cc6ae98cf7fe08468c3d55f396dc22ba61
Paulo Moutinho <paulo_vicali@icloud.com>
2023-02-02 17:44:26 -0300

    feat: atualiza dados estruturados

--- components/SchemaJson.js
+++ components/SchemaJson.js
@@ -4,15 +4,17 @@ const schemaData =
 {
   "@context": "https://schema.org/", 
   "@type": "Portifólio", 
+  "url": "https://paulomoutinho.vercel.app/",
   "name": "Paulo Moutinho",
   "image": "/public/assets/paulomoutinho.jpg",
-  "keywords": "Software Engineer",
+  "jobTitle": "Software Engineer",
   "category": "Front End",
-  "about": "Sou um desenvolvedor iniciando uma jornada no mundo da programação e suas tecnologias incríveis. Atualmente, trabalho como Software Engineer Junior na compania Via...",
+  "description": "Sou um desenvolvedor iniciando uma jornada no mundo da programação e suas tecnologias incríveis. Atualmente, trabalho como Software Engineer Junior na compania Via...",
   "brand": "Via",
   "author": {
     "@type": "Person",
-    "name": "Paulo Moutinho"
+    "name": "Paulo Moutinho",
+    "email": "mailto:paulo_vicali@icloud.com"
   }
 }
 
